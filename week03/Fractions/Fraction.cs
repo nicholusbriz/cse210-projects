b2641c9b0aa4ff1cf8405cb87a18acb99b5dcca0
nicholusbriz <atbriz256@gmail.com>
2025-09-17 16:30:40 +0300

    Fraction Project

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -1,62 +1,36 @@
+using System;
+
 public class Fraction
 {
     private int _top;
     private int _bottom;
 
-    // Constructor: no parameters, defaults to 1/1
     public Fraction()
     {
         _top = 1;
         _bottom = 1;
     }
 
-    // Constructor: one parameter (top), bottom defaults to 1
-    public Fraction(int top)
+    public Fraction(int wholeNumber)
     {
-        _top = top;
+        _top = wholeNumber;
         _bottom = 1;
     }
 
-    // Constructor: two parameters (top and bottom)
     public Fraction(int top, int bottom)
     {
         _top = top;
         _bottom = bottom;
     }
 
-    // Getter for top
-    public int GetTop()
-    {
-        return _top;
-    }
-
-    // Setter for top
-    public void SetTop(int top)
-    {
-        _top = top;
-    }
-
-    // Getter for bottom
-    public int GetBottom()
-    {
-        return _bottom;
-    }
-
-    // Setter for bottom
-    public void SetBottom(int bottom)
-    {
-        _bottom = bottom;
-    }
-
-    // Returns fraction as string (e.g., "3/4")
     public string GetFractionString()
     {
-        return $"{_top}/{_bottom}";
+        string text = $"{_top}/{_bottom}";
+        return text;
     }
 
-    // Returns decimal value
     public double GetDecimalValue()
     {
-        return (double)_top / _bottom;
+        return (double)_top / (double)_bottom;
     }
 }
